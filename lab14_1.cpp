79289b0ab463894b20672fe789bbdbca2c728d74
Suppakron <suwannapop01@gmail.com>
2023-01-13 14:42:38 +0700

    oky

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,38 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum=0 , sumSD=0 ,Harmonic=0,Geometric = 1 ;
+    double max = A[0], min= A[0];
+    for (int i = 0; i < N; i++)     
+    {
+        if (A[i]>max)
+        {
+            max = A[i];
+        }
+        if (A[i]<min)
+        {
+            min = A[i];
+        }
+    }
+    B[4] = max ;
+    B[5] = min ;
+    for (int i = 0; i < N; i++) 
+    {
+        sum += A[i]; 
+    }
+    
+    B[0] = sum/N ;
+    for (int i = 0; i < N; i++)
+    {
+        sumSD += pow(A[i]- B[0],2);
+        Harmonic += 1/A[i];
+        Geometric *= A[i];
+    }
+    B[1] = sqrt(sumSD/N);
+    B[3] = N/Harmonic ;
+    B[2] = pow(Geometric,(double)1/N);
+
+
+    
+}
